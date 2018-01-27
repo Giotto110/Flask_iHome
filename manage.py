62099283b934dae7c60846d738ceedd0779b00c6
giotto95827 <765675259@qq.com>
2018-01-27 16:54:49 +0800

    集成mysql数据库

--- manage.py
+++ manage.py
@@ -1,12 +1,19 @@
 # -*- coding:utf-8 -*-
 
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
 
 class Config(object):
     DEBUG = True
 
+    SQLAlchemy_DATABASE_URI = "mysql://root:mysql@127.0.0.1:3306/ihome"
+
 app = Flask(__name__)
 app.config.from_object(Config)
+
+
+db = SQLAlchemy(app)
 
 @app.route('/')
 def index():
