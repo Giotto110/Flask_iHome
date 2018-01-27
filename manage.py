3d33e368797616a6c29706f5f1e1626808bf1c27
giotto95827 <765675259@qq.com>
2018-01-27 19:03:55 +0800

    flask_session和manage数据库迁移集成

--- manage.py
+++ manage.py
@@ -1,10 +1,12 @@
 # -*- coding:utf-8 -*-
 
-from flask import Flask
+from flask import Flask,session
 from flask_sqlalchemy import SQLAlchemy
 import redis
-from flask_wtf.csrf import CsrfProtect
-
+from flask_wtf.csrf import CSRFProtect
+from flask_session import Session
+# from flask_script import Manager
+from flask_migrate import MigrateCommand,Migrate,Manager
 
 class Config(object):
 
@@ -12,11 +14,16 @@ class Config(object):
 
     DEBUG = True
 
-    SQLAlchemy_DATABASE_URI = "mysql://root:mysql@127.0.0.1:3306/ihome"
-    SQLAlchemy_TRACK_MODIFICATIONS =False
+    SQLALCHEMY_DATABASE_URI = "mysql://root:mysql@127.0.0.1:3306/ihome"
+    SQLALCHEMY_TRACK_MODIFICATIONS =False
 
     REDIS_HOST = "127.0.0.1"
     REDIS_PORT = 6379
+
+    SESSION_TYPE = "redis"
+    SESSION_REDIS = redis.StrictRedis(host=REDIS_HOST,port=REDIS_PORT)
+    SESSION_USE_SIGNER = True
+    PERMANENT_SESSION_LIFETIME = 86400*2
 
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -26,13 +33,20 @@ db = SQLAlchemy(app)
 
 redis_store = redis.StrictRedis(host=Config.REDIS_HOST,port=Config.REDIS_PORT)
 
-CsrfProtect(app)
+CSRFProtect(app)
+
+Session(app)
+
+manager = Manager(app)
+Manager(app,db)
+manager.add_command("db",MigrateCommand)
 
 @app.route('/',methods=["GET","POST"])
 def index():
     # redis_store.set("name","laowang")
+    session["name"] = "xiaohua"
     return 'index'
 
 
 if __name__ == '__main__':
-    app.run()
+    manager.run()
