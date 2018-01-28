6d3171625672e79cc90a1bc86feab1dde27cc0d2
giotto95827 <765675259@qq.com>
2018-01-28 18:57:42 +0800

    数据库迁移

--- manage.py
+++ manage.py
@@ -1,12 +1,12 @@
 # -*- coding:utf-8 -*-
 
-from iHome import create_app,db
+from iHome import create_app,db,models
 # from flask_script import Manager
 from flask_migrate import MigrateCommand, Migrate, Manager
 
 app = create_app("development")
 manager = Manager(app)
-Manager(app, db)
+Migrate(app, db)
 manager.add_command("db", MigrateCommand)
 
 
