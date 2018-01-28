16f0bc91cabc0b9fe6b075296f4b3b3232d2e707
giotto95827 <765675259@qq.com>
2018-01-28 16:32:19 +0800

    按照接口版本号进行蓝图目录区分

--- iHome/__init__.py
+++ iHome/__init__.py
@@ -7,6 +7,7 @@ from config import Config
 from flask_wtf.csrf import CSRFProtect
 from flask_session import Session
 from config import config
+from iHome.api_1_0 import api
 
 db = SQLAlchemy()
 
@@ -22,22 +23,6 @@ def create_app(config_name):
 
     Session(app)
 
-    return app
-
-def create_app(config_name):
-    """创建应用实例"""
-    app = Flask(__name__)
-
-
-    # 向app中添加自定义的路由转换器
-    app.url_map.converters['re'] = RegexConverter
-
-    # 注册蓝图，在使用的时候再引入
-    from iHome.api_1_0 import api
-    app.register_blueprint(api, url_prefix='/api/v1.0')
-
-    # 注册html静态文件的蓝图
-    import web_html
-    app.register_blueprint(web_html.html)
+    app.register_blueprint(api)
 
     return app
--- manage.py
+++ manage.py
@@ -10,7 +10,5 @@ Manager(app, db)
 manager.add_command("db", MigrateCommand)
 
 
-
-
 if __name__ == '__main__':
     manager.run()
