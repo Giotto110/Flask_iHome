05acfd535453cb7ed9cbabb47eae30e144e9e9b2
giotto95827 <765675259@qq.com>
2018-01-30 16:19:59 +0800

    注册前端逻辑

--- iHome/api_1_0/passport.py
+++ iHome/api_1_0/passport.py
@@ -7,38 +7,42 @@ from iHome.models import User
 from iHome.utils.response_code import RET
 from . import api
 
-@api.route('/users',method=["POST"])
+@api.route('/users',methods=["POST"])
 def register():
-    data_dict =request.json
-    mobile =data_dict.get("mobile")
+    data_dict = request.json
+    mobile = data_dict.get("mobile")
     phonecode = data_dict.get("phonecode")
     password = data_dict.get("password")
-    if not all([mobile,phonecode,password]):
-        return jsonify(errno=RET.PARAMERR,errmsg="参数错误")
+    # 判断参数是否都有值
+    if not all([mobile, phonecode, password]):
+        return jsonify(errno=RET.PARAMERR, errmsg="参数错误")
 
+    # 2. 取到真实的短信验证码
     try:
-        real_phonecode = redis_store.get("Mobile" + mobile)
+        real_phonecode = redis_store.get("Mobile:" + mobile)
     except Exception as e:
         current_app.logger.error(e)
-        return jsonify(errno=RET.DBERR,errmsg="查询短信验证码失败")
+        return jsonify(errno=RET.DBERR, errmsg="查询短信验证码失败")
 
     if not real_phonecode:
-        return jsonify(errno=RET.NODATA,errmsg="短信验证码过期")
+        return jsonify(errno=RET.NODATA, errmsg="短信验证码过期")
 
+    # 3. 进行验证码的对比
     if phonecode != real_phonecode:
-        return jsonify(errno=RET.DATAERR,errmsg="短信验证码输入错误")
+        return jsonify(errno=RET.DATAERR, errmsg="短信验证码输入错误")
 
-    user =User()
+    # 4. 初始化User模型，保存相关数据
+    user = User()
     user.mobile = mobile
     user.name = mobile
     # TODO save password
 
-    try:
-        db.session.add(user)
-        db.session.commit()
-    except Exception as e:
-        db.session.rollback()
-        current_app.logger.error(e)
-        return jsonify(errno=RET.DBERR,errmsg="保存用户数据失败")
+    # try:
+    #     db.session.add(user)
+    #     db.session.commit()
+    # except Exception as e:
+    #     db.session.rollback()
+    #     current_app.logger.error(e)
+    #     return jsonify(errno=RET.DBERR,errmsg="保存用户数据失败")
 
     return jsonify(errno=RET.OK,errmsg="注册成功")
--- iHome/static/js/ihome/register.js
+++ iHome/static/js/ihome/register.js
@@ -50,11 +50,21 @@ function sendSMSCode() {
         $(".phonecode-a").attr("onclick", "sendSMSCode();");
         return;
     }
+
+    // 通过ajax方式向后端接口发送请求，让后端发送短信验证码
+    var params = {
+        "mobile": mobile,
+        "image_code": imageCode,
+        "image_code_id": imageCodeId
+    }
+
+
+
     $.ajax({
         url: "/api/v1.0/sms_code",  // 请求地址
         type: "post",               // 请求方式
-        data: JSON.stringify(params), // 请求参数
-        header:{
+        data: JSON.stringify(params),// 请求参数
+        headers: {
             "X-CSRFToken": getCookie("csrf_token")
         },
         contentType: "application/json",// 请求参数的数据类型
@@ -65,7 +75,7 @@ function sendSMSCode() {
                 var num = 60
 
                 var t = setInterval(function () {
-                    if (num == 0) {
+                    if (num ==0) {
                         // 代表倒计时完成
                         // 清除定时器
                         clearInterval(t)
@@ -73,7 +83,7 @@ function sendSMSCode() {
                         $(".phonecode-a").html("获取验证码")
                         // 把点击事件添加回去
                         $(".phonecode-a").attr("onclick", "sendSMSCode();");
-                    } else {
+                    }else {
                         // 正在倒计时
                         // 去设置倒计时的秒数
                         $(".phonecode-a").html(num + "秒")
@@ -82,7 +92,7 @@ function sendSMSCode() {
                     num = num - 1
                 }, 1000)
 
-            } else {
+            }else {
                 // 把点击事件添加回去
                 $(".phonecode-a").attr("onclick", "sendSMSCode();");
                 // 重新生成图片验证码
@@ -90,15 +100,7 @@ function sendSMSCode() {
                 alert(resp.errmsg)
             }
         }
-    });
-
-
-    // 通过ajax方式向后端接口发送请求，让后端发送短信验证码
-    var params = {
-        "mobile": mobile,
-        "image_code": imageCode,
-        "image_code_id": imageCodeId
-    }
+    })
 }
 
 $(document).ready(function() {
@@ -119,6 +121,65 @@ $(document).ready(function() {
     $("#password2").focus(function(){
         $("#password2-err").hide();
     });
+    // 注册的提交(判断参数是否为空)
 
-    // TODO: 注册的提交(判断参数是否为空)
+    $(".form-register").submit(function (e) {
+        // 阻止默认的提交操作
+        e.preventDefault()
+
+        // 取值
+        var mobile = $("#mobile").val()
+        var phonecode = $("#phonecode").val()
+        var password = $("#password").val()
+        var password2 = $("#password2").val()
+
+        if (!mobile) {
+            $("#mobile-err span").html("请填写正确的手机号！");
+            $("#mobile-err").show();
+            return
+        }
+
+        if (!phonecode) {
+            $("#mphone-code-err span").html("请填写短信验证码！");
+            $("#phone-code-err").show();
+            return
+        }
+
+        if (!password) {
+            $("#password-err span").html("请填写密码!");
+            $("#password-err").show();
+            return;
+        }
+        if (password != password2) {
+            $("#password2-err span").html("两次密码不一致!");
+            $("#password2-err").show();
+            return;
+        }
+
+        var params = {
+            "mobile": mobile,
+            "phonecode": phonecode,
+            "password": password
+        }
+
+        $.ajax({
+            url: "/api/v1.0/users",
+            type: "post",
+            data: JSON.stringify(params),
+            headers: {
+                "X-CSRFToken": getCookie("csrf_token")
+            },
+            contentType: "application/json",
+            success: function (resp) {
+                if (resp.errno == "0") {
+                    //注册成功，进入到主页去
+                    location.href = "/"
+                }else {
+                    $("#password2-err span").html(resp.errmsg);
+                    $("#password2-err").show();
+                }
+            }
+        })
+
+    })
 })
