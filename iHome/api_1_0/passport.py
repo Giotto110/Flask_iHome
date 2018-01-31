2cee6854371044f6cdbeccd0629e3f4953e8b4fa
giotto95827 <765675259@qq.com>
2018-01-31 15:33:39 +0800

    登录后端逻辑实现

--- iHome/api_1_0/passport.py
+++ iHome/api_1_0/passport.py
@@ -1,11 +1,43 @@
 # -*- coding:utf-8 -*-
+import logging
 from flask import current_app
 from flask import request, jsonify
+from flask import session
 
 from iHome import redis_store, db
 from iHome.models import User
 from iHome.utils.response_code import RET
 from . import api
+
+@api.route('/session',methods=["POST"])
+def login():
+    dict_json = request.get_json()
+    mobile = dict_json.get('mobile')
+    password = dict_json.get('password')
+
+    if not all([mobile,password]):
+        return jsonify(errno=RET.PARAMERR,errmsg="参数不完整")
+
+    if not re.match(u"^1[345678]\d{9}$",mobile):
+        return jsonify(errno=RET.PARAMERR,errmsg="手机号码格式不正确")
+
+    try:
+        user = User.query.filter_by(mobile=mobile).first()
+    except Exception as e:
+        logging.error(e)
+        return jsonify(errno=RET.DBERR,errmsg='数据库查询错误')
+
+    if user is None:
+        return jsonify(errno=RET.USERERR,errmsg='用户不存在')
+
+    if not user.check_password(password):
+        return jsonify(errno=RET.LOGINERR,errmgs="密码错误")
+
+    session['user_id']=user.id
+    session['mobile']=user.mobile
+    session['name']=user.name
+
+    return jsonify(errno=RET.OK,errmsg='登录成功')
 
 @api.route('/users',methods=["POST"])
 def register():
