76ff561c752f21fad089dd1a24ab85004c78d22f
giotto95827 <765675259@qq.com>
2018-01-31 23:04:53 +0800

    用户个人中心数据展示

--- iHome/utils/image_storage.py
+++ iHome/utils/image_storage.py
@@ -1,6 +1,6 @@
 # -*- coding:utf-8 -*-
 
-from qiniu import Auth, put_data
+import qiniu
 
 access_key = "yV4GmNBLOgQK-1Sn3o4jktGLFdFSrlywR2C-hvsW"
 secret_key = "bixMURPL6tHjrb8QKVg2tm7n9k8C7vaOeQ4MEoeW"
@@ -9,19 +9,24 @@ bucket_name = "ihome"
 
 
 def upload_image(data):
-    q = Auth(access_key, secret_key)
-    token = q.upload_token(bucket_name)
-    ret, info = put_data(token, None, data)
+    """
+    上传图片到七牛云的方法
+    :param data: 图片数据
+    :return:
+    """
 
+    q = qiniu.Auth(access_key, secret_key)
+    token = q.upload_token(bucket_name)
+    ret, info = qiniu.put_data(token, None, data)
+    # 判断如果上传成功，那么就返回key
     if info.status_code == 200:
-        # 代表上传成功
         return ret.get("key")
     else:
-        # 代表上传失败
-        raise Exception("七牛上传文件失败")
+        # 如果上传失败，就抛出异常
+        raise Exception("上传图片失败")
 
 
 if __name__ == '__main__':
-    file_name = raw_input("请输入文件名：")
+    file_name = raw_input("请输入图片地址：")
     with open(file_name, "rb") as f:
-        print upload_image(f.read())
+        upload_image(f.read())
