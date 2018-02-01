c52214db529e803fb055d20f192f7b24791378e4
giotto95827 <765675259@qq.com>
2018-02-01 19:56:56 +0800

    新建房屋信息前端逻辑

--- iHome/static/js/ihome/newhouse.js
+++ iHome/static/js/ihome/newhouse.js
@@ -23,8 +23,38 @@ $(document).ready(function () {
         }
     })
 
-    // TODO: 处理房屋基本信息提交的表单数据
+    // 处理房屋基本信息提交的表单数据
+    $("#form-house-info").submit(function (e) {
+        e.preventDefault()
 
+        var params = {}
+
+        $(this).serializeArray().map(function (x) {
+            params[x.name] = x.value
+            // console.log(x)
+        })
+        var facilities = []
+        $(":checkbox:checked[name=facility]").each(function (index,x) {
+            facilities[index] = x.value
+        })
+        params["facility"] = facilities
+        console.log(params)
+        $.ajax({
+            url:"/api/v1.0/house",
+            type:"post",
+            contentType:"application/json",
+            headers:{
+                "X-CSRFToken":getCookie("csrf_token")
+            },
+            data:JSON.stringify(params),
+            success:function (resp) {
+                if (resp.errno == "0"){
+                    $("#form-house-info").hide()
+                    $("#form-house-image").show()
+                }
+            }
+        })
+    })
     // TODO: 处理图片表单的数据
 
 })
